f620661edf2a720e820a35aa06cbfeae14ad1a66
shubham-Nirmal <shubhamnirmal2612@gmail.com>
2025-05-14 20:29:17 -0700

    add new code

---
+++ AssociativeAarry_02.php
@@ -0,0 +1,19 @@
+<?php
+$age =  array(
+    "bill"  => 25,
+    "steve"  => 28,
+    "elon"   =>  22
+  
+);
+echo "<pre>";
+print_r($age);
+
+
+echo "<pre>";
+
+echo $age["bill"]. "<br>";
+echo $age["steve"]. "<br>";
+echo $age["elon"]. "<br>";
+
+
+?>
