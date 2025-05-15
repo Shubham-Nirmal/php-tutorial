18dca1fc47df7d84287dfc36f91c89c1471f5f67
shubham-Nirmal <shubhamnirmal2612@gmail.com>
2025-05-14 20:35:16 -0700

    Added new files

---
+++ Arry_01.php
@@ -0,0 +1,23 @@
+<?php 
+
+$colors =  ["red", 20, "blue ", 12.50 ];
+
+
+echo $colors [0];
+
+echo $colors [1]."<br>";
+echo $colors [2]."<br>";
+echo $colors [3]."<br>";
+
+
+
+
+
+
+/*
+for ($i=0; $i < 4  ; $i++) { 
+    echo $colors[$i]."<br>";
+} 
+    */
+
+?>
