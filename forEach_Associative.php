24e3d58fdde347502a69abbdfcc2ecc4d75e3bc4
shubham-Nirmal <shubhamnirmal2612@gmail.com>
2025-05-22 01:22:20 -0700

    first commit

---
+++ forEach_Associative.php
@@ -0,0 +1,16 @@
+<?php 
+$age  = [
+    "Bill"  => 25,
+    "steve" => 28,
+    "elon"  => 22,
+];
+
+
+/* foreach ($age as $key => $value) {
+    echo $value ."<br>";
+}
+*/
+foreach ($age  as $key => $value) {
+    echo $key. " = ". $value . "<br>";
+}
+?>
